e6abf7d0f9666fd008229232d8a89fa24577be6c
Waleed Lugod <lugodwaleed@gmail.com>
2023-09-30 21:12:54 +0800

    comments

--- guitar.py
+++ guitar.py
@@ -12,15 +12,18 @@ if __name__ == "__main__":
     for key_idx in range(len(keyboard)):
         strings[key_idx] = GuitarString(440 * 1.059463 ** (key_idx - 12))
 
+    # strings being simulated
     activeStrings = set()
 
     n_iters = 0
     while True:
+        # reduce bottleneck
         if n_iters == 1000:
             stdkeys.poll()
             n_iters = 0
         n_iters += 1
 
+        # process typed keys
         if stdkeys.has_next_key_typed():
             key = stdkeys.next_key_typed()
             index = keyboard.find(key)
@@ -28,15 +31,20 @@ if __name__ == "__main__":
                 strings[index].pluck()
                 activeStrings.add(strings[index])
 
+        # compute superposition of samples and cull simulated strings
         sample = 0
         newActiveStrings = activeStrings.copy()
         for string in activeStrings:
             sample += string.sample()
+
+            # stop simulating string after 80000 ticks have passed since it was first plucked
             if string.ticks_after_plucked() > 80000:
                 newActiveStrings.remove(string)
         activeStrings = newActiveStrings
 
+        # play sample
         play_sample(sample)
 
+        # advance simulation of currently active strings
         for string in activeStrings:
             string.tick()
