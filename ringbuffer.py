ca197f1133ae028ac1aee53a0371c965499a8596
jooooooosef <106436709+jooooooosef@users.noreply.github.com>
2023-09-30 14:46:02 +0800

    ringbuffer update
    ringbuffer is done!

--- ringbuffer.py
+++ ringbuffer.py
@@ -7,44 +7,91 @@ class RingBuffer:
         '''
         # TO-DO: implement this
         self.MAX_CAP = capacity
-        self._front = # to-do
-        self._rear = # to-do
+        self._front = -1
+        self._rear = -1
+        self.buffer = [None] * capacity
 
     def size(self) -> int:
         '''
         Return number of items currently in the buffer
         '''
-        # TO-DO: implement this
-
+        size = 0
+        for x in self.buffer:
+            if x != None:
+                size += 1
+        return size
+        '''
+        if self._rear > self._front:
+            return (self._rear-self._front+1)
+        elif self._rear < self._front:		# the rear is past the capacity
+            return (self.MAX_CAP-self._front+1) + (self._rear+1)
+        else:
+            return(0)
+        '''
     def is_empty(self) -> bool:
         '''
         Is the buffer empty (size equals zero)?
         '''
         # TO-DO: implement this
+        return (self.size() == 0)
         
     def is_full(self) -> bool:
         '''
         Is the buffer full (size equals capacity)?
         '''
         # TO-DO: implement this
+        return (self.size() == self.MAX_CAP)
 
     def enqueue(self, x: float):
         '''
         Add item `x` to the end
         '''
         # TO-DO: implement this
+        if (self.size() == self.MAX_CAP):
+            raise RingBufferFull
+        elif (self._front == -1):
+            self._front += 1
+            self._rear += 1
+        elif (self._rear == self.MAX_CAP-1):
+            self._rear = 0
+        else:
+            self._rear += 1
+            
+        self.buffer[self._rear] = x
 
     def dequeue(self) -> float:
         '''
         Return and remove item from the front
         '''
+        
         # TO-DO: implement this
+        
+        
+        
+        if (self.size() == 0):
+            raise RingBufferEmpty
+        
+        to_remove = self.buffer[self._front]
+        self.buffer[self._front] = None
+        
+        if (self._front == self.MAX_CAP-1):
+            self._front = 0
+        else:
+            self._front += 1
+            
+        
+        return to_remove
+        
 
     def peek(self) -> float:
         '''
         Return (but do not delete) item from the front
         '''
         # TO-DO: implement this
+        if self.size()==0:
+            raise RingBufferEmpty
+        else:
+            return (self.buffer[self._front])
 
 
 class RingBufferFull(Exception):
@@ -52,3 +99,4 @@ class RingBufferFull(Exception):
 
 class RingBufferEmpty(Exception):
     pass
+
